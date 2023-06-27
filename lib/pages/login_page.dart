402c161cc8bd1485f72fc7a31e5c1de83cead4a8
Luis Miguel Reyes Rodriguez <reyshut@Luis-Reyes-MacBook-Pro.local>
2023-06-26 21:41:04 -0500

    login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:minimarket_remi/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../home_page.dart';
 
 class LoginPage extends StatelessWidget {
-  static const String routeName = '/login';
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> login(BuildContext context) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+
+      // Inicio de sesión exitoso, realizar acciones necesarias aquí
+      print('Inicio de sesión exitoso: ${userCredential.user!.email}');
+
+      // Navegar a la siguiente página después del inicio de sesión (por ejemplo, Home)
+      // Navigator.pushNamed(context, '/home');
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => new HomePage())
+      );
+
+    } catch (e) {
+      // Error de inicio de sesión, mostrar mensaje de error o realizar otras acciones de manejo de errores
+      print('Error de inicio de sesión: $e');
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Error de inicio de sesión'),
+            content: Text(
+                'Ocurrió un error durante el inicio de sesión. Por favor, verifica tus credenciales e intenta nuevamente.'),
+            actions: [
+              TextButton(
+                child: Text('Cerrar'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,42 +56,43 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Inicio de sesión'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/logo.jpg',
-              width: 200.0,
-              height: 200.0,
-              fit: BoxFit.contain,
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Correo electrónico',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.symmetric(vertical: 32.0),
+                child: Image.asset(
+                  'assets/images/logo.jpg',
+                  height: 150.0,
+                ),
               ),
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Contraseña',
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Correo electrónico',
+                  prefixIcon: Icon(Icons.email),
+                ),
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 32.0),
-            ElevatedButton(
-              child: Text('Iniciar sesión'),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                );
-              },
-            ),
-          ],
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () => login(context),
+                child: Text('Iniciar sesión'),
+              ),
+            ],
+          ),
         ),
       ),
     );
